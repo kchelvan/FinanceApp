2c397a112fdea2a94d44155f8817ff3a83a2fb57
ronaldsin <ronald.sin@ontariotechu.net>
2020-03-21 13:16:49 -0400

    added various options to client server communication

--- src/main/java/financeClient.java
+++ src/main/java/financeClient.java
@@ -1,5 +1,8 @@
 package main.java;
 
+import main.java.helpers.Account;
+import main.java.helpers.User;
+
 import java.io.DataInputStream;
 import java.io.DataOutputStream;
 import java.io.IOException;
@@ -11,6 +14,11 @@ public class financeClient extends Thread{
     private DataInputStream fromServer;
     private DataOutputStream toServer;
 
+    private User user;
+
+    /**
+     * Starts the client and connects to server
+     */
     public void run(){
         System.out.println("Client starting...");
         do{
@@ -32,8 +40,178 @@ public class financeClient extends Thread{
         try {
             fromServer = new DataInputStream(soc.getInputStream());
             toServer = new DataOutputStream(soc.getOutputStream());
+
         } catch (IOException e) {
             e.printStackTrace();
         }
+    }
+
+    /**
+     * Request a user login to the server
+     *
+     * @param username The inputted username of the user
+     * @param password The inputted password of the user
+     * @return Either a error message or a success message
+     * @throws IOException
+     */
+    public String login(String username, String password) throws IOException {
+        toServer.writeUTF("login");
+
+        toServer.writeUTF(username);
+        toServer.writeUTF(password);
+
+        String res = getResult();
+        /*
+        res can be:
+        error(Invalid username): "Username Not Found !!!"
+        error(wrong password): "Invalid Login"
+        success: username + "," + password + "," + appData.get(username)
+         */
+        setUser(res);
+
+        return res;
+    }
+
+    /**
+     * Request to register a new user to the server
+     *
+     * @param username The inputted username of the new user
+     * @param password The inputted password of the new user
+     * @return Either a error message or a success message
+     * @throws IOException
+     */
+    public String register(String username, String password) throws IOException {
+        toServer.writeUTF("register");
+
+        toServer.writeUTF(username);
+        toServer.writeUTF(password);
+
+        String res = getResult();
+        /*
+        res can be:
+        error(already a user with this username): "User already Exists"
+        success: "User Added Sucessfully"
+         */
+
+        if(res.equals("User Added Sucessfully")){
+            setUser(res);
+        }
+        return res;
+    }
+
+    /**
+     * Adds an new blank account to the current user
+     *
+     * @param accountType The type of account
+     * @param accountName The account name
+     * @return A success message
+     * @throws IOException
+     */
+    public String addAccount(String accountType, String accountName) throws IOException {
+        Account account = new Account();
+        account.addAccount(accountType, accountName);
+
+        user.addAccount(account);
+
+        System.out.println("Account added");
+        return "Account added";
+    }
+
+    /**
+     * Deposits money into the selected account
+     *
+     * @param accountName The name of the target account
+     * @param value The value to be added
+     * @return A success message
+     * @throws IOException
+     */
+    public String deposit(String accountName, double value) throws IOException {
+        for(Account account : user.accountList){
+            if(account.getAccountName().equals(accountName)){
+                account.deposit(value);
+                System.out.println("Deposited");
+                break;
+            }
+        }
+
+        return "Deposited";
+    }
+
+    /**
+     * Withdraws money from the selected account
+     *
+     * @param accountName The name of the target account
+     * @param value The value to be taken out
+     * @return A success message
+     * @throws IOException
+     */
+    public String withdraw(String accountName, double value) throws IOException {
+        for(Account account : user.accountList){
+            if(account.getAccountName().equals(accountName)){
+                account.withdraw(value);
+                System.out.println("Withdrew");
+                break;
+            }
+        }
+
+        return "Withdrew";
+    }
+
+    /**
+     * Request the server to save user data into a csv
+     *
+     * @return A success message
+     * @throws IOException
+     */
+    public String save() throws IOException {
+        toServer.writeUTF("save");
+
+        int size = user.accountList.size();
+        toServer.writeInt(size);
+
+        for(Account accounts : user.accountList) {
+            toServer.writeUTF(accounts.toString());
+        }
+
+        String res = getResult();
+        return res;
+    }
+
+    /**
+     * Safely end the connection to server
+     *
+     * @throws IOException
+     */
+    public void exit() throws IOException {
+        toServer.writeUTF("exit");
+
+        soc.close();
+    }
+
+    /**
+     * creates a user based on given data
+     *
+     * @param data The data the user is to be created with
+     */
+    public void setUser(String data){
+        if(!data.equals("Username Not Found !!!") || !data.equals("Invalid Login")){
+            user = new User(data);
+        }
+    }
+
+    /**
+     * gets a result message from the server
+     *
+     * @return The result message from the server
+     * @throws IOException
+     */
+    public String getResult() throws IOException {
+        String res = fromServer.readUTF();
+        System.out.println(res);
+        return res;
+    }
+
+    public User getUser(){
+        return user;
     }
 }
--- src/main/java/server/financeServerThread.java
+++ src/main/java/server/financeServerThread.java
@@ -1,38 +1,130 @@
 package main.java.server;
 
+import main.java.helpers.Account;
+import main.java.helpers.TempDatabase;
+import main.java.helpers.User;
+
 import java.io.DataInputStream;
 import java.io.DataOutputStream;
 import java.io.IOException;
 import java.net.Socket;
 
 public class financeServerThread extends Thread {
+    private financeServer fs;
+
     private Socket soc;
 
     private DataInputStream fromClient;
     private DataOutputStream toClient;
 
-    financeServerThread(Socket soc) throws IOException {
+    private TempDatabase db;
+
+    /**
+     * The constructor for the thread
+     * Thread allows for multiple users to connect at once
+     *
+     * @param soc The socket that listens for a new user
+     * @param fs The server object
+     * @throws IOException
+     */
+    financeServerThread(Socket soc, financeServer fs) throws IOException {
         System.out.println("Starting user thread...");
 
+        this.fs = fs;
         this.soc = soc;
 
         fromClient = new DataInputStream(soc.getInputStream());
         toClient = new DataOutputStream(soc.getOutputStream());
+
+        db = new TempDatabase();
     }
 
+    /**
+     * Runs the thread and listens for client instructions
+     */
     public void run() {
         System.out.println("User thread running...");
+        System.out.println("Waiting for instructions...");
+
+        while(true){
+            try {
+                String task = fromClient.readUTF();
+                decideTask(task);
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
     }
 
-    public void closeSocket(){ // closes the socket
-        System.out.println("Closing socket...");
-        try{
-            System.out.println("Socket closed...");
-            soc.close();
+    /**
+     * Decides what instructions to execute
+     *
+     * @param task The name of the task it is to execute
+     * @throws IOException
+     */
+    public void decideTask(String task) throws IOException {
+        if(task.equals("login")){
+            login();
         }
-        catch(IOException e){
-            e.printStackTrace();
-            System.out.println("Socket failed to close...");
+        else if(task.equals("register")){
+            register();
+        }
+        else if(task.equals("save")){
+            save();
+        }
+        else if(task.equals("exit")){
+            closeSocket();
         }
     }
+
+    /**
+     * Logs the user in with data given by client
+     *
+     * @throws IOException
+     */
+    public void login() throws IOException {
+        String username = fromClient.readUTF();
+        String password = fromClient.readUTF();
+
+        String res = db.login(username, password);
+        toClient.writeUTF(res);
+    }
+
+    /**
+     * Adds a new user based on data from the client
+     *
+     * @throws IOException
+     */
+    public void register() throws IOException {
+        String username = fromClient.readUTF();
+        String password = fromClient.readUTF();
+
+        String res = db.addUser(username, password);
+        toClient.writeUTF(res);
+    }
+
+    /**
+     * Saves user data into a csv based on data from client
+     *
+     * @throws IOException
+     */
+    public void save() throws IOException {
+        int size = fromClient.readInt();
+
+        for(int i = 0; i < size; i++){
+            db.saveDatabase(fromClient.readUTF());
+        }
+
+        toClient.writeUTF("Saved");
+    }
+
+    /**
+     * Closes the socket
+     */
+    public void closeSocket(){
+        System.out.println("Closing socket...");
+        fs.removeUser(this);
+    }
+
+
 }
