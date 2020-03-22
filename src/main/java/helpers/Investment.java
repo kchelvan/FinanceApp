1f3eb0b87585954fc4581ef0c1fa28ec19075b17
sinthujan <sinthujan.ramanathan@ontariotechu.net>
2020-03-22 14:25:21 -0400

    Changed years from integer to double

--- src/main/java/helpers/Investment.java
+++ src/main/java/helpers/Investment.java
@@ -7,36 +7,36 @@ public class Investment{
     private double initialInvestment;
     private double investmentGoal;
     private double growthRate;
-    private int years;
+    private double years;
 
     // Investment goal calculator
-    public void investmentFV(double initialInvestment, double growthRate, int years){
+    public void investmentFV(double initialInvestment, double growthRate, double years){
         this.initialInvestment = initialInvestment;
         this.growthRate = growthRate;
         this.years = years;
 
         //this.investmentGoal = initialInvestment * Math.pow((1+growthRate/1200),(double)years*12);
-        this.investmentGoal = initialInvestment * Math.pow((1 + growthRate),(double) years);
+        this.investmentGoal = initialInvestment * Math.pow((1 + growthRate), years);
     }
     
     // Investment initial value calculator
-    public void investmentIV(double investmentGoal, double growthRate, int years){
+    public void investmentIV(double investmentGoal, double growthRate, double years){
         this.investmentGoal = investmentGoal;
         this.growthRate = growthRate;
         this.years = years;
 
         //this.initialInvestment = investmentGoal / Math.pow((1+growthRate/1200),(double)years*12);
-        this.initialInvestment = investmentGoal / Math.pow(1 + growthRate,(double)years);
+        this.initialInvestment = investmentGoal / Math.pow(1 + growthRate,years);
     }
 
     // Investment Growth Rate Calculator
-    public void investmentGR(double initialInvestment, double investmentGoal, int years){
+    public void investmentGR(double initialInvestment, double investmentGoal, double years){
         this.initialInvestment = initialInvestment;
         this.investmentGoal = investmentGoal;
         this.years = years;
 
         //this.growthRate = (Math.pow(initialInvestment / investmentGoal, 1 / (double)years * 12) - 1) * 1200;
-        this.growthRate = Math.pow(investmentGoal/initialInvestment, 1 / (double) years) - 1;
+        this.growthRate = Math.pow(investmentGoal/initialInvestment, 1 / years) - 1;
 
     }
 
@@ -46,12 +46,12 @@ public class Investment{
         this.investmentGoal = investmentGoal;
         this.growthRate = growthRate;
 
-        this.years = (int) Math.ceil(Math.log(investmentGoal/initialInvestment)/Math.log(1+growthRate));
+        this.years = Math.log(investmentGoal/initialInvestment)/Math.log(1+growthRate);
     }
 
     // Accessors for the class
     public double getInitialInvestment(){ return this.initialInvestment;}
     public double getInvestmentGoal(){ return this.investmentGoal;}
     public double getGrowthRate(){ return this.growthRate;}
-    public int getYears(){ return this.years;}
+    public double getYears(){ return this.years;}
 }
